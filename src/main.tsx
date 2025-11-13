938cb31b07051c61915301bf56d5c6264424af37
Paulo Kazuaki <pkmmna.snf24@uea.edu.br>
2025-11-13 11:48:31 -0400

    fix/resolvendo conflitos com Development

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,8 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login/login";
 import Home from "./pages/home/home";
 import Register from "./pages/register/register";
-import Profile from "./pages/profile/profile";
+import Profile from "./pages/profile/profile"; // veio da tela-login
+import ServiceRequest from "./pages/serviceRequest/ServiceRequest"; // veio da development
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -14,10 +15,12 @@ createRoot(document.getElementById("root")!).render(
       <Routes>
         {/* rota inicial: login */}
         <Route path="/" element={<Login />} />
-        {/* sua outra tela */}
+        
         <Route path="/home" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/service-requests" element={<ServiceRequest />} />
+
         {/* qualquer rota desconhecida volta pro login */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
